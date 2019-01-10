8f245e46926482f6ea379d0b4d1922341154479a
gustavoweb33 <lealgustavo33@gmail.com>
2019-01-09 22:05:24 -0600

    modified courses array to display credit hours

--- src/components/SemesterTables.js
+++ src/components/SemesterTables.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import '../containers/SemesterTables.css';
+//import courses from '../courses';
 
 const SemesterTables = (props) => {
+    //console.log(props.courses)
     const userChosenSemester = props.semesterValue;
     const userChosenCourses = props.chosenCourses;
+
+    const filteredCourses = [];
+    for (let i = 0; i < props.courses.length; i++) {
+        if (userChosenCourses.includes(props.courses[i].courseId)) {
+            filteredCourses.push(props.courses[i])
+        }
+    }
+
 
     return (
         <div className='grid-container'>
             <div>
                 <h3>Smester:  {userChosenSemester}</h3>
 
-                {
+                {/*
                     userChosenCourses.map((course, index) => {
                         return (
                             <div key={index}>
                                 <li key={course}> {course}</li>
-                                <button value={course} onClick={props.delete}>Delete</button>
+                                <button value={course} onClick={props.deleteCourse}>Delete</button>
                             </div>
                         )
                     })
-                }
+               */}
+                <div>
+                    {
+                        filteredCourses.map((course, index) => {
 
+                            return (
+                                <div key={index}>
+                                    <li key={course.courseId}> {course.courseId} ({course.creditHours})</li>
+                                    <button value={course.courseId} onClick={props.deleteCourse}>Delete</button>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
