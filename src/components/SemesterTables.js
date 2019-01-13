6114adabfb1584c4e3cf20939037b114336972e4
gustavoweb33 <lealgustavo33@gmail.com>
2019-01-12 23:02:57 -0600

    added error warning to file

--- src/components/SemesterTables.js
+++ src/components/SemesterTables.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../containers/SemesterTables.css';
-//import courses from '../courses';
+import style from '../containers/errorStyles.module.css';
 
 const SemesterTables = (props) => {
-    //console.log(props.courses)
+    let warningStyle = null;
+    const errorMessage = <p>You have surpassed the maximum credit hours allowed</p>;
+
     const userChosenSemester = props.semesterValue;
     const userChosenCourses = props.chosenCourses;
     let totalCreditHours = 0;
@@ -15,18 +17,17 @@ const SemesterTables = (props) => {
         }
     }
 
-   
 
     filteredCourses.forEach(course => totalCreditHours += course.creditHours)
-    
-    if(totalCreditHours >= 27) {
-        alert('You have exceded to maximum number of hours')
+
+    if (totalCreditHours >= 21) {
+        warningStyle = style.error;
     }
 
     return (
         <div className='grid-container'>
             <div>
-                <h3>Smester:  {userChosenSemester}</h3>
+                <h3>Semester:  {userChosenSemester}</h3>
 
                 {/*
                     userChosenCourses.map((course, index) => {
@@ -38,7 +39,7 @@ const SemesterTables = (props) => {
                         )
                     })
                */}
-                <div>
+                <div className={style.tableGrid}>
                     {
                         filteredCourses.map((course, index) => {
 
@@ -51,8 +52,9 @@ const SemesterTables = (props) => {
                         })
                     }
                 </div>
-                <h3>Total Credit Hours: {totalCreditHours}</h3>
 
+                <h3 className={warningStyle}>Total Credit Hours: {totalCreditHours}</h3>
+                <span className={style.errorMessage}>{totalCreditHours >= 21 ? errorMessage : null}</span>
             </div>
         </div>
     );
